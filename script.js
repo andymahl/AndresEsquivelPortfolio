3e9056237dd5e7f4289ca71d0fb75c7119e75560
andymahl <122275414+andymahl@users.noreply.github.com>
2024-01-23 11:45:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function loadResume() {
+    // Ocultar la sección de contenido principal
+    document.querySelector('.content').style.display = 'none';
+    // Mostrar la sección del currículum
+    document.querySelector('.resume-section').style.display = 'block';
+}
